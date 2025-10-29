0be58cb82b4d29e45de8876964e810443dbc4c02
Funyinoluwa Kashimawo <funyin.kash@gmail.com>
2025-10-29 03:54:51 +0100

    version upgrade

--- FormValidator/build.gradle.kts
+++ FormValidator/build.gradle.kts
@@ -98,7 +98,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.funyin"
             artifactId = "formvalidator"
-            version = "1.0.1"
+            version = "1.0.2"
 
 //            afterEvaluate {
 //                from(components["release"])
