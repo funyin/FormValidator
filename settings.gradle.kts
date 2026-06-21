38cf1e53b8a9680c277b4ceefd45d3a75bda75ef
Funyinoluwa Kashimawo <funyin.kash@gmail.com>
2026-06-21 17:46:13 +0100

    minimal change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,9 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
